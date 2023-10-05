2f49737e7b076815ff14468b755a2936b2b52d2f
Talyia Mira <Tali.km.angel@gmail.com>
2023-10-06 02:46:25 +0300

    declared/initialized variables and ran them

---
+++ variables.dart
@@ -0,0 +1,17 @@
+void main() {
+  int number = 50;
+  String name = "Talyia";
+  bool isSmart = true;
+  List<String> colors = ["Red", "Blue", "Pink", "Purple"];
+  List<Map<int, String>> id_name = [
+    {1: "Talyia"},
+    {2: "Ryam"}
+  ];
+
+  print('Number: $number');
+  print('Name: $name');
+  print('Smart?: $isSmart');
+  print('Color: $colors');
+  print('Id with name: $id_name');
+}
+
