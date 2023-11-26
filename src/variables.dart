13d8110405bc68e75e73235314ee396d6abf97a8
HASSAN ALASHOOR <37499427+alashoor89@users.noreply.github.com>
2023-11-27 00:33:58 +0300

    Error Handling in dart

--- src/variables.dart
+++ src/variables.dart
@@ -1,11 +1,21 @@
-void main() {
-  int age = 30;
-  String name = 'John';
-  bool isStudent = true;
-  List<String> fruits = ['apple', 'banana', 'cherry'];
+// void main() {
+//   int age = 30;
+//   String name = 'John';
+//   bool isStudent = true;
+//   List<String> fruits = ['apple', 'banana', 'cherry'];
 
-  print('Age: $age');
-  print('Name: $name');
-  print('Is Student: $isStudent');
-  print('Fruits: $fruits');
+//   print('Age: $age');
+//   print('Name: $name');
+//   print('Is Student: $isStudent');
+//   print('Fruits: $fruits');
+// }
+
+void main() {
+  try {
+    // ~/ integer divison like // in python
+    int result = 10 ~/ 0;
+    print('Result: $result');
+  } catch (e) {
+    print('An error occurred: $e');
+  }
 }
