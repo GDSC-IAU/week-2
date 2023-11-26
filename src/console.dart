f908f5a4c0abbae022c75199f5cd34f2f210574c
HASSAN ALASHOOR <37499427+alashoor89@users.noreply.github.com>
2023-11-27 01:11:16 +0300

    Printing Welcome message

--- src/console.dart
+++ src/console.dart
@@ -6,7 +6,7 @@ void main() {
   stdout.write('Enter your age: ');
   // tryParse when error happens it will return null otherwise the user input
   int? age = int.tryParse(stdin.readLineSync()!) ?? 0;
-  // ?? tells dart if the left operand is null then use this value 
+  // ?? tells dart if the left operand is null then use this value (right operand) 
   
   print("Your age is: $age");
 }
--- src/main.dart
+++ src/main.dart
@@ -1,7 +1,39 @@
 // TODO: Run task list app
+
+import 'dart:io';
+
+final List<TaskListApp> tasks = [];
+
 void main(List<String> args) {
-  print('Hello World');
+  // attacking radwan,
+// I am newbie in programming, what is loop?
+
+  print('------------------ Task Manager ------------------');
+  print('------ Welcome to our beautfuil Task Manager -----');
+  print('------  Please enter the menu option below  ------');
+  print('--------------------------------------------------');
+
+  print('--- 1 for "List Task"');
+  print('--- 2 for "Add Task"');
+  print('--- 3 for "Edit Task"');
+  print('--- 4 for "Delete Task"');
+
+  while (true) {
+    print('Enter valid option: ');
+    int option = int.tryParse(stdin.readLineSync()!) ?? -1;
+
+    switch (option) {
+      default:
+        print('Invalid option');
+    }
+  }
 }
+
+void addTask() {}
+
+void editTask() {}
+
+void deleteTask() {}
 
 class TaskListApp {
   // TODO: Implement Task list app
