723d21b18ef4b5f1dae4555d6156c984c369787d
HASSAN ALASHOOR <37499427+alashoor89@users.noreply.github.com>
2023-11-27 00:29:25 +0300

    Adding variable.dart

---
+++ src/variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  int age = 30;
+  String name = 'John';
+  bool isStudent = true;
+  List<String> fruits = ['apple', 'banana', 'cherry'];
+
+  print('Age: $age');
+  print('Name: $name');
+  print('Is Student: $isStudent');
+  print('Fruits: $fruits');
+}
