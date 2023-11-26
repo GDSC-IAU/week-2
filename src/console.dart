5577ed7a108cdcfeae7badb3ded2a0a19a397a3a
HASSAN ALASHOOR <37499427+alashoor89@users.noreply.github.com>
2023-11-27 00:54:55 +0300

    Converting Console Input to other datatypes Part

--- src/console.dart
+++ src/console.dart
@@ -1,11 +1,11 @@
-import 'dart:convert';
 import 'dart:io';
 
 void main() {
-  stdout.write("Enter your name: ");
-  String? name = stdin.readLineSync(
-    encoding: Encoding.getByName("UTF-8")!,
-  );
+  // refer to back commits to see what happend here.
 
-  print(name);
+  stdout.write('Enter your age: ');
+  // can result in error if user put non integer
+  int? age = int.parse(stdin.readLineSync()!);
+
+  print("Your age is: $age");
 }
