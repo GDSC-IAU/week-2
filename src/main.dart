40e74c7c3410414b9a3f64f193d5159b993c8977
ferhad_Baaqieel <ferhad.baaqieel@gmail.com>
2023-12-01 14:47:28 +0300

    deleted varibels

--- src/main.dart
+++ src/main.dart
@@ -1,8 +1,8 @@
-// TODO: Run task list app
+TODO: Run task list app
 void main(List<String> args) {
   print('Hello World');
 }
 
 class TaskListApp {
-  // TODO: Implement Task list app
+  TODO: Implement Task list app
 }
